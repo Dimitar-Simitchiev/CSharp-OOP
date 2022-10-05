4ec270f10bb64e0dfdfeb9508b2d0ea7525b3a21
Dimitar Simitchiev <51047171+Dimitar-Simitchiev@users.noreply.github.com>
2022-10-05 11:02:21 +0300

    Add files via upload

---
+++ UseAtribute/ConsoleApp8/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp8
+{
+    [Author("Mitko")]
+    internal class Program
+    {
+        [Author("Ivan")]
+        static void Main(string[] args)
+        {
+            Console.WriteLine("HI");
+
+        }
+    }
+    [AttributeUsage(AttributeTargets.Class |AttributeTargets.Method,AllowMultiple =true)]
+    public class AuthorAttribute : Attribute
+    {
+        public AuthorAttribute(string name)
+        {
+            this.Name = name;
+        }
+
+        public string Name { get; }
+    }
+}
